cd3fbb1235e431888e41c4ca91cf73eecf7186bd
Arda <arda.akdere@hotmail.com>
2023-05-10 23:36:01 +0300

    Second commit

--- logic/urls.py
+++ logic/urls.py
@@ -4,7 +4,9 @@ from logic import views
 app_name = "logic"
 
 urlpatterns = [
-    path("", views.Home.as_view()),
+    path("", views.Home.as_view(), name="home"),
     path("table/", views.Table.as_view(), name="table_content"),
     path("delete-course/", views.course_delete, name="delete_course"),
+    path('update-course/<str:pk>/', views.update_course, name = "update-course"),
+    path('create-course/', views.create_course, name = "create-course"),
 ]
--- logic/views.py
+++ logic/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from logic import models
 from django.http import HttpResponse
+from logic import forms
 
 # Create your views here.
 class Home(View):
@@ -33,3 +34,29 @@ def course_delete(request):
     models.Course.objects.get(pk=course_pk).delete()
 
     return HttpResponse('200')
+
+
+def update_course(request, pk):
+    course = models.Course.objects.get(id=pk)
+    form = forms.CourseForm(instance=course)
+    
+    if request.method == 'POST':
+        form = forms.CourseForm(request.POST, instance=course)
+        if form.is_valid():
+            form.save()
+            return redirect('logic:home')
+        
+    context = {'form' : form}
+    return render(request, 'logic/course_form.html', context)
+
+
+def create_course(request):
+    form = forms.CourseForm()
+    if request.method == 'POST':
+        form = forms.CourseForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('logic:home')
+            
+    context = {'form' : form}
+    return render(request, 'logic/course_form.html', context)
